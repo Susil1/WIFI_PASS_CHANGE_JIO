2ac66126f92126f8500b9a2dbeb201ee5a97f892
Susilcreation68 <98740340+Susil1@users.noreply.github.com>
2025-12-10 15:54:21 +0530

    added password change feature

--- main.py
+++ main.py
@@ -133,16 +133,16 @@ class routerConnection:
         )
         return logout_response.json().get("status","NOT_OK")
     
-    def getInfo(self,method,params=None):
+    def getInfo(self,method=None,params=None,info_payload=None):
         HEADERS = {
             "Cookie": f"cSupport=1; sysauth={self.sysauth}",
             "Authorization": f"Bearer {self.token}",
         }
-
-        info_payload = Payload(
-            method = method,
-            params = params if params else {},
-        )
+        if (not info_payload):
+            info_payload = Payload(
+                method = method if method else "",
+                params = params if params else {},
+            )
 
         info_response = requests.post(
             URL,
@@ -156,9 +156,32 @@ class routerConnection:
         else:
             return info_response.json()
     def changePassword(self,newpass):
-        with open("newpass.txt","w") as file:
-            file.write(newpass)
-        print(f"New Password: {newpass}")
+        wireless_config=self.getInfo("getWirelessConfiguration")
+        if (isinstance(wireless_config,dict) and wireless_config["status"] != "OK"):
+            raise ConnectionError("Error Getting WirelessConfiguration!")
+        if isinstance(wireless_config,dict):
+            results = wireless_config["results"]
+            ssid = results["ssid"]
+            security = results["security"]
+            recordId = results["recordId"]
+            pararms={
+                "recordId":recordId,
+                "ssid":ssid,
+                "security":security,
+                "password":newpass
+                }
+            passchange_payload = Payload(
+                method="setWirelessConfiguration",
+                params=pararms
+            )
+            passchange_response = self.getInfo(info_payload=passchange_payload)
+            if (isinstance(passchange_response,dict) and passchange_response["status"]=="OK"):
+                print("Password Successfully Changed")
+                with open("newpass.txt","w") as file:
+                    file.write(newpass)
+                print(f"New Password: {newpass}")
+        else:
+            raise ValueError("Type Missmatch")
     def capture_packet(self,interface,size,file_name="capture.pcap"):
         self.getInfo("startCapturePackets",params={"interface":interface,"size":size})
         print("Packet Capture Started.")
@@ -179,7 +202,8 @@ def main():
     status=connection.initialise_connection()
     if (status=="OK"):
         print("Logged in succsesfully.")
-    connection.changePassword(getNewPassword())
+    newpass= getNewPassword()
+    connection.changePassword(newpass)
     # connection.capture_packet(interface="any",size=5)
     # pprint(connection.getInfo("getLanClients"))
     # pprint(connection.getInfo("getCpuUtilisation"))
